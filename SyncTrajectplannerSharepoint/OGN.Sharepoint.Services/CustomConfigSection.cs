6a36c732a61200f8d9880296907338b8746deb42
gmiValueBlue <gerard.michels@valueblue.nl>
2014-07-23 15:11:38 +0200

    complete

---
+++ SyncTrajectplannerSharepoint/OGN.Sharepoint.Services/CustomConfigSection.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Configuration;
+
+namespace OGN.Sharepoint.Services
+{
+
+    public class SitePermissionsSection : ConfigurationSection
+    {
+        [ConfigurationProperty("permissions", IsDefaultCollection = false)]
+        [ConfigurationCollection(typeof(SitePermissions),
+            AddItemName = "add")]
+        public SitePermissions Permissions
+        {
+            get
+            {
+                SitePermissions perms =
+                    (SitePermissions)base["permissions"];
+                return perms;
+            }
+        }
+
+    }
+
+    public class SitePermissions : ConfigurationElementCollection
+    {
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new PermissionBindingConfigElement();
+        }
+
+        protected override Object GetElementKey(ConfigurationElement element)
+        {
+            PermissionBindingConfigElement elem = (PermissionBindingConfigElement)element;
+            return elem.SiteGroup+elem.Permission;
+        }
+
+        public PermissionBindingConfigElement this[int index]
+        {
+            get
+            {
+                return (PermissionBindingConfigElement)BaseGet(index);
+            }
+            set
+            {
+                if (BaseGet(index) != null)
+                {
+                    BaseRemoveAt(index);
+                }
+                BaseAdd(index, value);
+            }
+        }
+    }
+
+    public class PermissionBindingConfigElement : ConfigurationElement
+    {
+        [ConfigurationProperty("sitegroup", IsRequired = true)]
+        public string SiteGroup
+        {
+            get
+            {
+                return (string)this["sitegroup"];
+            }
+            set
+            {
+                this["sitegroup"] = value;
+            }
+        }
+
+        [ConfigurationProperty("permission", IsRequired = true)]
+        public string Permission
+        {
+            get
+            {
+                return (string)this["permission"];
+            }
+            set
+            {
+                this["permission"] = value;
+            }
+        }
+
+    }
+}
--- SyncTrajectplannerSharepoint/OGN.Sharepoint.Services/SyncEduSitesService.svc.cs
+++ SyncTrajectplannerSharepoint/OGN.Sharepoint.Services/SyncEduSitesService.svc.cs
@@ -10,29 +10,31 @@ using System.ServiceModel.Web;
 using System.Text;
 using System.Net;
 using System.Diagnostics;
+using System.Configuration;
 
 namespace OGN.Sharepoint.Services
 {
     public class SyncEduSitesService : ISyncEduSitesService
     {
-        private string _home_url = "http://acc-teamwise.ogn.eu/";
+        //app settings (see constructor)
+        private string _home_url;
         private NetworkCredential _creds;
-        private Guid _loi_id = new Guid("6f5a554e-4667-46eb-aabd-9739b1c5a8d6");
-        private Guid _cat_id = new Guid("a571a3e6-ac38-442e-8ea5-c98bc3cbaaf2");
-        private Guid _mod_id = new Guid("c68d43c8-bee0-4cf4-a1d4-f8329710ae91");
-        private Guid _edu_id = new Guid("95adde66-646d-4b27-9242-0972360881bf");
-      //  private string _webtemplate = "{581CD673-C0EE-4069-B067-4CB6F61A6BDD}#ModuleSite";
-        private string _webtemplate = "{E88B612A-DC40-4ECE-8DE4-964C6F2D7767}#OpleidingModuleSiteTemplate";
-        private string _link2edu_list = "Links";
-        private string _link2mod_list = "Links";
+        private Guid _loi_id;
+        private Guid _cat_id;
+        private Guid _mod_id;
+        private Guid _edu_id;
+        private string _modtemplate;
+        private string _edutemplate;
+        //private string _webtemplate = "{E88B612A-DC40-4ECE-8DE4-964C6F2D7767}#OpleidingModuleSiteTemplate";
+        private string _link2edu_list;
+        private string _link2mod_list;
 
         //PowerShell: New-EventLog -LogName Application -Source OGN_Sharepoint_Services_SyncEduSitesService
         private string _eventlogsource = "OGN_Sharepoint_Services_SyncEduSitesService";
 
         public SyncEduSitesService()
         {
-            //get settings
-                    
+            //get settings       
             SecureString _pass = new SecureString();
             _pass.AppendChar('V');
             _pass.AppendChar('a');
@@ -42,7 +44,20 @@ namespace OGN.Sharepoint.Services
             _pass.AppendChar('3');
             _pass.AppendChar('l');
             _pass.AppendChar('u');
-            _creds = new NetworkCredential("gmichels", _pass, "ad");
+            //the credentials of the application pool are used.
+            _creds = CredentialCache.DefaultNetworkCredentials; 
+            //_creds = new NetworkCredential("gmichels", _pass, "ad"); //for testing
+
+            _home_url = ConfigurationManager.AppSettings["sp.sitecollection:url"];
+            _loi_id = new Guid(ConfigurationManager.AppSettings["sp.termstore:id"]);
+            _cat_id = new Guid(ConfigurationManager.AppSettings["sp.termstore.termset:id"]);
+            _mod_id = new Guid(ConfigurationManager.AppSettings["sp.termstore.termset.modset:id"]);
+            _edu_id = new Guid(ConfigurationManager.AppSettings["sp.termstore.termset.eduset:id"]);
+            _modtemplate = ConfigurationManager.AppSettings["sp.modsite:template"];
+            _link2edu_list = ConfigurationManager.AppSettings["sp.modsite:list2edu"];
+            _edutemplate = ConfigurationManager.AppSettings["sp.edusite:template"];
+            _link2mod_list = ConfigurationManager.AppSettings["sp.edusite:list2mod"];
+            _eventlogsource = ConfigurationManager.AppSettings["eventlogsource"];
         }
 
         #region ErrorHandlingAndLogging
@@ -109,12 +124,12 @@ namespace OGN.Sharepoint.Services
             ctx.ExecuteQuery();
         }
 
-        private void CreateSite(ClientContext ctx, ICourseTemplate edumod)
+        private void CreateSite(ClientContext ctx, ICourseTemplate edumod, string template)
         {
             Web site = ctx.Web;
 
             WebCreationInformation newsite = new WebCreationInformation();
-            newsite.WebTemplate = _webtemplate;
+            newsite.WebTemplate = template;
             newsite.Title = edumod.GetTitle();
             newsite.Url = edumod.Code;
             newsite.UseSamePermissionsAsParentSite = true;
@@ -122,12 +137,55 @@ namespace OGN.Sharepoint.Services
             ctx.ExecuteQuery();
         }
 
+        private bool SiteExists(ClientContext ctx, ICourseTemplate edumod)
+        {
+            Web site = ctx.Web;
+
+            ctx.Load(site.Webs, sites => sites.Include(subsite => subsite.Url));
+            ctx.ExecuteQuery();
+
+            return 0<site.Webs.Count(subsite => subsite.Url.EndsWith("/" + edumod.Code));
+        }
+
+
         private void ChangeTitle(ClientContext ctx, ICourseTemplate edumod)
         {
             Web site = ctx.Web;
             site.Title = edumod.GetTitle();
             site.Update();
             ctx.ExecuteQuery();
+        }
+
+        private void ChangePermissions(ClientContext ctx, ICourseTemplate edumod)
+        {
+            Web site = ctx.Web;
+            site.BreakRoleInheritance(false,false);
+            /* BreakRoleInheritance(copyRoleAssignments,clearSubscopes)
+             * copyRoleAssignments
+             *   Type: System.Boolean
+             *   Specifies whether to copy the role assignments from the parent securable object.
+             *   If the value is false, the collection of role assignments must contain only 1 role assignment containing the current user after the operation.
+             * clearSubscopes
+             *   Type: System.Boolean
+             *   If the securable object is a site, and the clearsubscopes parameter is true, the role assignments for all child securable objects in the current site and in the sites which inherit role assignments from the current site must be cleared and those securable objects will inherit role assignments from the current site after this call.
+             *   If the securable object is a site, and the clearsubscopes parameter is false, the role assignments for all child securable objects which do not inherit role assignments from their parent object must remain unchanged.
+             */
+            ctx.ExecuteQuery();
+            
+            SitePermissionsSection config = (SitePermissionsSection)ConfigurationManager.GetSection("sp.sitepermissions.deactivate");
+
+            foreach (PermissionBindingConfigElement item in config.Permissions)
+            {
+                Group sitegroup = site.SiteGroups.GetByName(item.SiteGroup);
+
+                RoleDefinition permission = site.RoleDefinitions.GetByName(item.Permission);
+                RoleDefinitionBindingCollection rdbs = new RoleDefinitionBindingCollection(ctx);
+
+                rdbs.Add(permission);
+                site.RoleAssignments.Add(sitegroup, rdbs);
+            }
+            ctx.ExecuteQuery();
+
         }
 
         private void CreateLink(ClientContext ctx, string listtitle, ICourseTemplate linkto)
@@ -144,6 +202,20 @@ namespace OGN.Sharepoint.Services
             
             item.Update(); 
             ctx.ExecuteQuery();
+        }
+
+        private bool LinkExists(ClientContext ctx, string listtitle, ICourseTemplate linkto)
+        {
+            Web site = ctx.Web;
+            List list = site.Lists.GetByTitle(listtitle);
+
+            CamlQuery qry = new CamlQuery();
+            //qry.ViewXml = "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>announce</Value></Eq></Where></Query></View>";
+            ListItemCollection items = list.GetItems(qry);
+            ctx.Load(items);
+            ctx.ExecuteQuery();
+
+            return 0 < items.Count(item => ((FieldUrlValue)item["URL"]).Url.Equals(linkto.GetUrl(_home_url)));
         }
 
         private void UpdateAllLinksToEduOrMod(ClientContext ctx, string listtitle, ICourseTemplate edumod)
@@ -164,7 +236,7 @@ namespace OGN.Sharepoint.Services
                 ClientContext ctx2 = this.GetSite(sitelinkstome.Url);
                 //there are two names for lists of links on an edu or mod sites, mod sites have links to edus, edu sites have links to mods.
                 //listtitle is one name, listtitle2 must be the other
-                string listtitle2 = (listtitle == _link2edu_list) ? _link2mod_list : _link2edu_list;
+                string listtitle2 = (listtitle.Equals(_link2edu_list)) ? _link2mod_list : _link2edu_list;
                 this.UpdateLink(ctx2, listtitle2, edumod);
             }
         }
@@ -184,7 +256,7 @@ namespace OGN.Sharepoint.Services
             foreach (ListItem item in items)
             {
                 FieldUrlValue url = (FieldUrlValue)item["URL"];
-                if (url.Url == linkto.GetUrl(_home_url))
+                if (url.Url.Equals(linkto.GetUrl(_home_url)))
                 {
                     url.Description = linkto.GetTitle();
                     item["URL"] = url;
@@ -210,7 +282,7 @@ namespace OGN.Sharepoint.Services
             foreach (ListItem item in items)
             {
                 FieldUrlValue url = (FieldUrlValue) item["URL"];
-                if (url.Url == linkto.GetUrl(_home_url)) 
+                if (url.Url.Equals(linkto.GetUrl(_home_url))) 
                 {
                     item.DeleteObject();
                     break;
@@ -238,10 +310,17 @@ namespace OGN.Sharepoint.Services
             {
                 report.Messages.Add("Maak opleiding: code->"+edu.Code+", naam->"+edu.Name);
                 ClientContext ctx = this.GetSite(_home_url);
-                this.CreateSite(ctx, edu);
-                report.Messages.Add("Site gemaakt.");
-                this.AddTerm(ctx, _edu_id, edu);
-                report.Messages.Add("Term gemaakt.");
+                if (this.SiteExists(ctx, edu))
+                {
+                    this.LogWarning("Site niet gemaakt. Opleiding bestaat al.", report);
+                }
+                else
+                {
+                    this.CreateSite(ctx, edu, _edutemplate);
+                    report.Messages.Add("Site gemaakt.");
+                    this.AddTerm(ctx, _edu_id, edu);
+                    report.Messages.Add("Term gemaakt.");
+                }
             }
             catch (Exception e) { this.LogException(e, "Er is een fout opgetreden tijdens operatie Create(edu):\n" + e.Message, report); }
             return report;
@@ -274,7 +353,8 @@ namespace OGN.Sharepoint.Services
             {
                 report.Messages.Add("Deactiveer opleidingssite: code->" + edu.Code + ", naam->" + edu.Name);
                 ClientContext ctx = this.GetSite(edu.GetUrl(_home_url));
-                throw new NotImplementedException("Archiveringssysteem nog niet bepaald.");
+                this.ChangePermissions(ctx, edu);
+                report.Messages.Add("Permissies ingetrokken.");
             }
             catch (Exception e) { this.LogException(e, "Er is een fout opgetreden tijdens operatie Delete(edu):\n" + e.Message, report); }
             return report;
@@ -288,10 +368,17 @@ namespace OGN.Sharepoint.Services
             {
                 report.Messages.Add("Maak module: code->" + mod.Code + ", naam->" + mod.Name);
                 ClientContext ctx = this.GetSite(_home_url);
-                this.CreateSite(ctx, mod);
-                report.Messages.Add("Site gemaakt.");
-                this.AddTerm(ctx, _mod_id, mod);
-                report.Messages.Add("Term gemaakt.");
+                if (this.SiteExists(ctx, mod))
+                {
+                    this.LogWarning("Site niet gemaakt. Module bestaat al.", report);
+                }
+                else
+                {
+                    this.CreateSite(ctx, mod, _modtemplate);
+                    report.Messages.Add("Site gemaakt.");
+                    this.AddTerm(ctx, _mod_id, mod);
+                    report.Messages.Add("Term gemaakt.");
+                }
             }
             catch (Exception e) { this.LogException(e, "Er is een fout opgetreden tijdens operatie Create(mod):\n" + e.Message, report); }
             return report;
@@ -323,7 +410,8 @@ namespace OGN.Sharepoint.Services
             {
                 report.Messages.Add("Deactiveer modulesite: code->" + mod.Code + ", naam->" + mod.Name);
                 ClientContext ctx = this.GetSite(mod.GetUrl(_home_url));
-                throw new NotImplementedException("Archiveringssysteem nog niet bepaald.");
+                this.ChangePermissions(ctx, mod);
+                report.Messages.Add("Permissies ingetrokken.");
             }
             catch (Exception e) { this.LogException(e, "Er is een fout opgetreden tijdens operatie Delete(mod):\n" + e.Message, report); }
             return report;
@@ -337,11 +425,25 @@ namespace OGN.Sharepoint.Services
             {
                 report.Messages.Add("Maak links: opl_code->" + link.EduProgramme.Code + ", mod_code->" + link.Module.Code);
                 ClientContext ctx_edu = this.GetSite(link.EduProgramme.GetUrl(_home_url));
-                this.CreateLink(ctx_edu, _link2mod_list, link.Module);
-                report.Messages.Add("Link naar modulesite gemaakt.");
+                if (this.LinkExists(ctx_edu, _link2mod_list, link.Module))
+                {
+                    this.LogWarning("Link naar modulesite niet gemaakt. Link bestaat al.", report);
+                }
+                else
+                {
+                    this.CreateLink(ctx_edu, _link2mod_list, link.Module);
+                    report.Messages.Add("Link naar modulesite gemaakt.");
+                }
                 ClientContext ctx_mod = this.GetSite(link.Module.GetUrl(_home_url));
-                this.CreateLink(ctx_mod, _link2edu_list, link.EduProgramme);
-                report.Messages.Add("Link naar opleidingssite gemaakt.");
+                if (this.LinkExists(ctx_mod, _link2edu_list, link.EduProgramme))
+                {
+                    this.LogWarning("Link naar opleidingssite niet gemaakt. Link bestaat al.", report);
+                }
+                else
+                {
+                    this.CreateLink(ctx_mod, _link2edu_list, link.EduProgramme);
+                    report.Messages.Add("Link naar opleidingssite gemaakt.");
+                }
             }
             catch (Exception e) { this.LogException(e, "Er is een fout opgetreden tijdens operatie Create(link):\n" + e.Message, report); }
             return report;
@@ -383,7 +485,7 @@ namespace OGN.Sharepoint.Services
         }
 
         public OperationReport Test()
-        {
+        {             
             OperationReport report = new OperationReport();
             try
             {
